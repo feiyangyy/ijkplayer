1ab416283215c1ba7d9ffcb764b3973e2fb2ffa9
qianlongxu <qianlongxu@gmail.com>
2025-03-13 10:16:44 +0800

    use androidx.appcompat.app.AppCompatActivity instead of android.support.v7.app.AppCompatActivity

--- android/ijkplayer/ijkplayer-example/src/main/java/tv/danmaku/ijk/media/example/activities/SettingsActivity.java
+++ android/ijkplayer/ijkplayer-example/src/main/java/tv/danmaku/ijk/media/example/activities/SettingsActivity.java
@@ -20,11 +20,11 @@ package tv.danmaku.ijk.media.example.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
 import tv.danmaku.ijk.media.example.R;
 import tv.danmaku.ijk.media.example.fragments.SettingsFragment;
 
