9c4fd54b6f814911c922fa610c21bf4b7d964893
qianlongxu <qianlongxu@gmail.com>
2025-03-13 10:19:09 +0800

    use androidx.annotation.NonNull instead of android.support.annotation.NonNull

--- android/ijkplayer/ijkplayer-example/src/main/java/tv/danmaku/ijk/media/example/fragments/TracksFragment.java
+++ android/ijkplayer/ijkplayer-example/src/main/java/tv/danmaku/ijk/media/example/fragments/TracksFragment.java
@@ -20,7 +20,7 @@ package tv.danmaku.ijk.media.example.fragments;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
