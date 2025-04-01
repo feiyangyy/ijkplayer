11dd96bdf243528902db2b373e34bb2869f71ce8
Matt Reach <qianlongxu@gmail.com>
2025-04-01 19:45:05 +0800

    Update make-podspec.sh

--- ios/xcframewrok/make-podspec.sh
+++ ios/xcframewrok/make-podspec.sh
@@ -18,7 +18,7 @@ THIS_DIR=$(DIRNAME=$(dirname "$0"); cd "$DIRNAME"; pwd)
 cd "$THIS_DIR"
 
 set -e
-version=${1:-$(grep -m 1 VERSION_NAME= ../../version.sh | awk -F = '{printf "k%s",$2}')}
+version=${1:-$(grep -m 1 VERSION_NAME= ../../version.sh | awk -F = '{printf "%s",$2}')}
 
 fn=IJKMediaFramework.spec.json
 cat 'template.spec.json' \
